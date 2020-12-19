4ba7632879264d34b13f154b302af3a0c41902a5
Mark Lukin <mark.lukin36@gmail.com>
2020-12-19 12:07:26 +0300

    An app version for Azure

--- src/main/resources/db/patches/v1.0-ddl-create-basic-tables.sql
+++ src/main/resources/db/patches/v1.0-ddl-create-basic-tables.sql
@@ -5,41 +5,41 @@
 
 CREATE TABLE tbl_user
 (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL
+    id INT IDENTITY PRIMARY KEY,
+    email NVARCHAR(255) NOT NULL UNIQUE,
+    first_name NVARCHAR(255) NOT NULL,
+    last_name NVARCHAR(255) NOT NULL,
+    password NVARCHAR(255) NOT NULL,
+    role NVARCHAR(255) NOT NULL
 );
 
 CREATE TABLE tbl_place_type
 (
-    id SERIAL PRIMARY KEY,
-    label VARCHAR(255) NOT NULL UNIQUE ,
-    icon VARCHAR(255),
-    description TEXT
+    id INT IDENTITY PRIMARY KEY,
+    label NVARCHAR(255) NOT NULL UNIQUE ,
+    icon NVARCHAR(255),
+    description NVARCHAR(1000)
 );
 
 CREATE TABLE tbl_map_marker
 (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY PRIMARY KEY,
     lng NUMERIC NOT NULL,
     lat NUMERIC NOT NULL,
-    label VARCHAR(255) NOT NULL,
-    description TEXT,
-    status VARCHAR(255) NOT NULL,
+    label NVARCHAR(255) NOT NULL,
+    description NVARCHAR(2000),
+    status NVARCHAR(255) NOT NULL,
     id_place_type INT NOT NULL,
     FOREIGN KEY (id_place_type) REFERENCES tbl_place_type(id)
 );
 
 CREATE TABLE tbl_post
 (
-    id SERIAL PRIMARY KEY,
-    label VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    date TIMESTAMP NOT NULL,
-    status VARCHAR(255) NOT NULL,
+    id INT IDENTITY PRIMARY KEY,
+    label NVARCHAR(255) NOT NULL,
+    description NVARCHAR(2000) NOT NULL,
+    date DATETIME NOT NULL,
+    status NVARCHAR(255) NOT NULL,
     id_place_type INT NOT NULL,
     id_author INT NOT NULL,
     id_map_marker INT,
@@ -50,12 +50,12 @@ CREATE TABLE tbl_post
 
 CREATE TABLE tbl_event
 (
-    id SERIAL PRIMARY KEY,
-    label VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    start_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP NOT NULL,
-    status VARCHAR(255) NOT NULL,
+    id INT IDENTITY PRIMARY KEY,
+    label NVARCHAR(255) NOT NULL,
+    description NVARCHAR(2000) NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
+    status NVARCHAR(255) NOT NULL,
     id_author INT NOT NULL,
     id_map_marker INT,
     FOREIGN KEY (id_author) REFERENCES tbl_user(id),
@@ -64,7 +64,7 @@ CREATE TABLE tbl_event
 
 CREATE TABLE tbl_attendance
 (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY PRIMARY KEY,
     id_user INT NOT NULL,
     id_event INT NOT NULL,
     FOREIGN KEY (id_user) REFERENCES tbl_user(id),
